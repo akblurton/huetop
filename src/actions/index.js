5204a07fae1e737f94b4afc774baeb7c8a1c8d67
Ashley Blurton <a.k@blurton.me>
2016-02-14 16:01:11 +0000

    Initial redux setup

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+// Export all other action sub-types
+export * from "./groups";
+export * from "./hubs";
+export * from "./lights";
+export * from "./rooms";
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import hub from "./hubs";
+
+export default combineReducers({
+  hub
+});
