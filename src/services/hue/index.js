40196cb457610552de6c788484db704e4ff8a1d3
Ashley Blurton <a.k@blurton.me>
2016-03-04 20:33:27 +0000

    Hue service spec for bridge list

---
+++ src/services/hue/index.js
@@ -0,0 +1,23 @@
+export const NUPNP_ADDRESS = "https://www.meethue.com/api/nupnp";
+
+import fetch from "../fetch";
+
+class Hue {
+  static _bridges = null;
+  static reset() {
+    Hue._bridges = null;
+  }
+  static bridges() {
+    if (Hue._bridges !== null) {
+      return Promise.resolve(Hue._bridges);
+    }
+
+    return fetch(NUPNP_ADDRESS)
+      .then(result => {
+        return Hue._bridges = result;
+      })
+      .catch(() => []);
+  }
+}
+
+export default Hue;
