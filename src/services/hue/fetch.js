346d9d9e8b0f4dfb8f6f7770bbe533a3264ccfa2
Ashley Blurton <a.k@blurton.me>
2016-03-09 20:48:56 +0000

    Convert fetch to async function

--- src/services/hue/fetch.js
+++ src/services/hue/fetch.js
@@ -1,32 +1,32 @@
-const fetch = (url, config = {}) => {
+const fetch = async (url, config = {}) => {
   if ("body" in config && config.body instanceof Object) {
     config = Object.assign({}, config, {
       "body": JSON.stringify(config.body)
     });
   }
-  return global.fetch(url, config)
-    .then(response => {
-      if (!response.ok) {
-        let error = new Error(response.statusText);
-        error.response = error;
+
+  let response = await global.fetch(url, config);
+  // Fail on non-2xx statuses
+  if (!response.ok) {
+    let error = new Error(response.statusText);
+    error.response = error;
+    throw error;
+  }
+
+  let result = await response.json();
+
+  // Handle Hue errors
+  if (result instanceof Array) {
+    for (let item of result) {
+      if (item.error) {
+        let error = new Error();
+        Object.assign(error, item.error);
         throw error;
       }
-      return response;
-    })
-    .then(response => response.json())
-    .then(result => {
-      // Handle Hue errors
-      if (result instanceof Array) {
-        for (let item of result) {
-          if (item.error) {
-            let error = new Error();
-            Object.assign(error, item.error);
-            throw error;
-          }
-        }
-      }
-      return result;
-    });
+    }
+  }
+
+  return result;
 };
 
 export default fetch;
